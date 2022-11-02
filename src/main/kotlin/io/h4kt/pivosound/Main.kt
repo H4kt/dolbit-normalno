33b9fbb699789a61742440cbf1caf6ec18f91a59
H4kt <h4ktoff@gmail.com>
2022-11-03 01:00:32 +0300

    added disconnect detection

--- src/main/kotlin/io/h4kt/pivosound/Main.kt
+++ src/main/kotlin/io/h4kt/pivosound/Main.kt
@@ -1,14 +1,19 @@
 package io.h4kt.pivosound
 
+import dev.kord.common.annotation.KordVoice
 import io.h4kt.pivosound.commands.*
 import dev.kord.core.Kord
+import dev.kord.core.event.user.VoiceStateUpdateEvent
+import dev.kord.core.on
 import dev.kord.gateway.Intent
 import dev.kord.gateway.Intents
 import dev.kord.gateway.PrivilegedIntent
+import io.h4kt.pivosound.managers.audioPlayer
+import io.h4kt.pivosound.managers.unregisterVoiceConnection
 
 const val DISCORD_TOKEN = "MTAzNzM5MDU1MzcwNjM5NzgyNw.Gbnihx.FY9tuhuzGuF6EtESyeopdaS7-g_XZ_MTZ_0smg"
 
-@OptIn(PrivilegedIntent::class)
+@OptIn(PrivilegedIntent::class, KordVoice::class)
 suspend fun main() {
 
     val kord = Kord(token = DISCORD_TOKEN)
@@ -19,6 +24,18 @@ suspend fun main() {
     CommandLeave.register(kord)
     CommandJoin.register(kord)
     CommandPlay.register(kord)
+
+    kord.on<VoiceStateUpdateEvent> {
+
+        val old = old
+        if ((old?.channelId != null && state.channelId == null) || (old?.channelId != null && state.channelId != null)) {
+            kord.unregisterVoiceConnection(old.guildId)?.apply {
+                audioPlayer?.destroy()
+                leave()
+            }
+        }
+
+    }
 
     kord.login {
         intents = Intents.nonPrivileged + Intent.MessageContent
